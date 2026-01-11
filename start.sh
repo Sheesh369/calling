4a465504a77468883278ee4f0e86870a164f81b9
Sheesh369 <shreeshashivakumar84@gmail.com>
2026-01-11 16:52:08 +0530

    Remove ngrok from start.sh - handled by docker-compose

--- start.sh
+++ start.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-# Configure ngrok with auth token
-ngrok config add-authtoken ${NGROK_AUTHTOKEN:-2wOMeC9CkWjnbjQa0bvC42cNLsR_617fw5AQqYQVpBdwpckkX}
-
-# Start ngrok in the background with your reserved domain
-ngrok http --url=seagull-winning-personally.ngrok-free.app 7860 &
-
-# Wait a moment for ngrok to start
-sleep 3
-
-# Start the Python server in the background (it will serve the built React app)
+# Start the Python server (ngrok is handled by docker-compose)
 python server.py &
 
 # Keep container running
